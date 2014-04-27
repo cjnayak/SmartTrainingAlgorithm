6bcbf0ebc1e2504e704642ec30bdb6757da67a83
gracegit <gracegoogoo@gmail.com>
2014-04-26 17:13:58 -0700

    added in tenure

--- runner.py
+++ runner.py
@@ -1,22 +1,26 @@
 import matplotlib.pyplot as plt
+import scipy.stats as stats
 import project_functions as pf
 import project_algorthims as pa
 
 #test gating algorthim
 if __name__ == "__main__":
-	pf.tenure("2013-01-22")
-	users, batch_score = pf.readData('data/Getty_Gold.json')
+	users, batch_score = pf.readData('data/Getty_Training1.json')
 
 	#run calculate_avg_score_per_batch on the global user scores
 	global_batch = pf.calculate_avg_score_per_batch(batch_score)
 
 	scores = {}
 	#this is the test parameter for a particular batch/project we are looking at so we exclude it from their aggregate score
-	exclude_batch = 892
+	exclude_batch = 517
 
+	user_ten = []
 	for user in users:
-		scores[user] = {}
-		scores[user]["batch"], scores[user]["av"] = pf.calc_user_performance(users[user]["batch"], global_batch, exclude_batch)
+		if user != 368:
+			if user != 369:
+				user_ten.append(users[user]["tenure"])
+				scores[user] = {}
+				scores[user]["batch"], scores[user]["av"] = pf.calc_user_performance(users[user]["batch"], global_batch, exclude_batch)
 
 	score_array = []
 	outliers = []
@@ -26,29 +30,19 @@ if __name__ == "__main__":
 			outliers.append(scores[score]["av"])
 		if scores[score]["av"] < -3.0:
 			outliers.append(scores[score]["av"])
+			print score
+
 	score_array.sort()
 	print "Outliers:"
 	print outliers
 
-
-	plt.hist(score_array, 100)
-	plt.ylabel('Frequency')
-	plt.xlabel('Average Score Distribution')
-	plt.title('Histogram of Performance Scores')
-	#plt.plot(bins, y, 'r--')
-	plt.show()
+	test_params = [score_array[10], 0.25612226458999177, -0.4, 0, 0, 0, 0.9, 10]
+	print pa.gatingFrequencyStepWise(*test_params)
+	print pa.gatingFrequencyStepWisePenalty(score_array[104], 0.25612226458999177, -0.4, 0, 0, 0, 0.9, 10, 0.5)
+	print pa.gatingFrequencyAttenuated(*test_params)
+	print pa.gatingFrequencyAttenuatedContinous(*test_params)
 
 
-	stw = pa.gatingFrequencyStepWise(score_array[104], 0.25612226458999177, -0.4, 0, 0, 0, 0.9, 10 )
-	stwpen = pa.gatingFrequencyStepWisePenalty(score_array[104], 0.25612226458999177, -0.4, 0, 0, 0, 0.9, 10, 0.5 )
-	atten = pa.gatingFrequencyAttenuated(score_array[104], 0.25612226458999177, -0.4, 0, 0, 0, 0.9, 10 )
-	attenCont = pa.gatingFrequencyAttenuatedContinous(score_array[104], 0.25612226458999177, -0.4, 0, 0, 0, 0.9, 10 )
-
-	print score_array[104]
-	print stw
-	print stwpen
-	print atten
-	print attenCont
 
 
 
