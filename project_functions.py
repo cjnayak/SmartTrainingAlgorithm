f97cd4ce3607835c1aeef3e020eab27a0071a0b9
cjnayak <chayajnayak@gmail.com>
2014-05-01 13:05:27 -0700

    Revert "committing additions to time"
    This reverts commit 2609e94afc729e2422c5089bb7782a7a8cd3bfe2.

--- project_functions.py
+++ project_functions.py
@@ -7,8 +7,6 @@ def readData(json_file):
 	    data = json.load(data_file)
 	users = {}
 	batch_score = {}
-	users_time = {}
-	batch_time = {}
 	for i in range(len(data)):
 		#exclude Sama Source HQ tests
 		if data[i]["delivery_centers.id"] != "1":
@@ -20,21 +18,14 @@ def readData(json_file):
 				corr = 0
 			uid = int(str(data[i]["users.id"]))
 			ten = tenure(data[i]["gold_metrics.answered_date"])
-			duration = int(str(data[i]["gold_metrics.duration"]))
-			
 			if uid not in users:
 				users[uid] = {"tasks": 0, "batch": {}, "tenure" : ten }	
 				#users[uid]["batch"].append(c_batchID)
 				#users[uid]["score"].append(corr)
-			
-			if uid not in users_time:
-				users_time[uid] = {"tasks": 0, "batch": {}, "tenure" : ten }
-				users_time[uid]["tasks"] += 1
-			
 			else:
 				if ten > users[uid]["tenure"]:
 					users[uid]["tenure"] = ten
-				users[uid]["tasks"] += 1
+			users[uid]["tasks"] += 1
 
 			if batchID in batch_score:
 				batch_score[c_batchID].append(corr)
@@ -47,22 +38,10 @@ def readData(json_file):
 			else:
 				users[uid]["batch"].setdefault(c_batchID, [])
 				users[uid]["batch"][c_batchID].append(corr)
-			
-			if batchID in batch_time:
-				batch_time[c_batchID].append(duration)
-							
-			else:
-				batch_time.setdefault(c_batchID, [])
-				batch_time[c_batchID].append(duration)
-			
-			if batchID in users_time[uid]["batch"]:
-				users_time[uid]["batch"][c_batchID].append(duration)
-			else:
-				users_time[uid]["batch"].setdefault(c_batchID, [])
-				users_time[uid]["batch"][c_batchID].append(duration)
-						
 
-	return users, batch_score, users_time, batch_time
+
+
+	return users, batch_score
 
 def calculate_avg_score_per_batch(batch_dict):
 	average_batch_score = {}
